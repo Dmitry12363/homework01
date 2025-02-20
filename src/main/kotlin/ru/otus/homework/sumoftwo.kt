b58f0ed5ee6e3e2a1d96741d7c32cc4007e75ca7
d.boldyrev <d.boldyrev@inpas.ru>
2025-02-20 11:37:55 +0300

    ДЗ homework01

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,6 +1,20 @@
 package ru.otus.homework
 
-
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
-    TODO("Выполните задание")
+    var i = 0
+    var j = 1
+
+    if (numbers.size < 2)
+        throw  IllegalArgumentException("size < 2")
+    while (i <= numbers.size-2) {
+        while (j <= numbers.size-1) {
+            if (numbers[i] + numbers[j] == target) {
+                return intArrayOf(numbers[i], numbers[j])
+            }
+            ++j
+        }
+        ++i
+        j = i + 1
+    }
+    throw IllegalArgumentException("Not found")
 }
