dc5f0391670731bf484194208dd0248d2e861c40
d.boldyrev <d.boldyrev@inpas.ru>
2025-02-20 11:58:22 +0300

    ДЗ homework01

--- src/main/kotlin/ru/otus/homework/sumoftwo.kt
+++ src/main/kotlin/ru/otus/homework/sumoftwo.kt
@@ -1,5 +1,11 @@
 package ru.otus.homework
 
+fun main() {
+    val arr : IntArray = intArrayOf(1, 2, 3, 4, 5)
+    val res : IntArray = sumOfTwo(arr, 9)
+    print("${res[0]}  ${res[1]}")
+}
+
 fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
     var i = 0
     var j = 1
@@ -9,7 +15,7 @@ fun sumOfTwo(numbers: IntArray, target: Int): IntArray {
     while (i <= numbers.size-2) {
         while (j <= numbers.size-1) {
             if (numbers[i] + numbers[j] == target) {
-                return intArrayOf(numbers[i], numbers[j])
+                return intArrayOf(i, j)
             }
             ++j
         }
